202934fef180c20bae6070fad30f3c2d538f493f
Jakub Szwacz <jakub@szwacz.com>
2013-12-05 21:40:09 +0100

    dropdown amended

--- app/directives/dropdown.js
+++ app/directives/dropdown.js
@@ -5,22 +5,21 @@ function dropdownDirective() {
         restrict: 'C',
         link: function ($scope, $element) {
             
-            var btn = angular.element($element.children()[0]);
-            
-            btn.bind('click', function () {
+            function toggle() {
                 $element.toggleClass('dropdown--opened');
-                process.nextTick(function () {
-                    document.addEventListener('click', hide, false);
-                });
-            });
-            
-            function hide() {
-                $element.toggleClass('dropdown--opened');
-                document.removeEventListener('click', hide, false);
+                if ($element.hasClass('dropdown--opened')) {
+                    setTimeout(function () {
+                        document.addEventListener('click', toggle, false);
+                    }, 50);
+                } else {
+                    document.removeEventListener('click', toggle, false);
+                }
             }
             
+            $element.bind('click', toggle);
+            
             $scope.$on('$destroy', function () {
-                document.removeEventListener('click', hide, false);
+                document.removeEventListener('click', toggle, false);
             });
         }
     };
